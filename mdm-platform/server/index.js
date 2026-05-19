54957c5629edb8cb8e799ccc6e63efd5442b9759
Harbinian <charlesarcher@outlook.com>
2026-05-19 15:04:08 +0800

    feat: add roles CRUD and permission matrix routes

--- mdm-platform/server/index.js
+++ mdm-platform/server/index.js
@@ -53,6 +53,7 @@ registerRouteIfExists('/api/attributes', 'attribute');
 registerRouteIfExists('/api/external', 'external');
 registerRouteIfExists('/api/integration', 'integration');
 registerRouteIfExists('/api/quality', 'quality');
+registerRouteIfExists('/api/roles', 'roles');
 
 app.get('/api/health', (req, res) => {
   res.json({ status: 'ok' });
